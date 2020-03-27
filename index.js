4ec510ecd6dedbe1c2e127b53bc5b7927708b7fa
Lauren Yu <laurenkyu@gmail.com>
2020-03-27 21:58:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-function titleCased(tutorial) {
+// function titleCased(tutorial) {
   
-  return tutorial;
-}
+//   return tutorial;
+// }
 
-const titleCased = (input) => {
-  return tutorials.map( line => {
-    let tokens = line.split(' ')
-    let capitalizedTokens =
-      tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1) )
-    let response = capitalizedTokens.join(' ')
-    return response
-  })
-}
+// const titleCased = (input) => {
+//   return tutorials.map( line => {
+//     let tokens = line.split(' ')
+//     let capitalizedTokens =
+//       tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1) )
+//     let response = capitalizedTokens.join(' ')
+//     return response
+//   })
+// }
 
 
-let newTutorials = tutorials.map(titleCased);
+// let newTutorials = tutorials.map(titleCased);
 
